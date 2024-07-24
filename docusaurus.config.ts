31c340a0b908ea39152848bc60249e419c1134b6
theRunCom <1772414249@qq.com>
2024-07-24 15:39:09 +0800

    Update blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,8 +55,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'MatrixFoundation',
       logo: {
@@ -70,7 +68,13 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          to: "/blog",
+          label: "Blogs",
+          position: "left",
+          className: "nav-blogs",
+          activeBasePath: "/blog",
+        },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -79,49 +83,13 @@ const config: Config = {
       ],
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      style: "light",
+      logo: {
+        alt: "Logo",
+        src: "img/logo.png",
+        width: 200,
+      },
+      copyright: `Made with ❤️ by <a target="_blank" rel="noopener noreferrer" href="https://github.com/MatrixFoundation">MatrixFoundation.</a>`,
     },
     prism: {
       theme: prismThemes.github,
