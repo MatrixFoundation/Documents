b127e00433b0834accc690dfa5e3f4a91f8f76a3
Nexseer <1772414249@qq.com>
2024-09-17 15:32:44 +0800

    Updated redirect path in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Layout from '@theme/Layout';
-import { Redirect } from "@docusaurus/router";
+import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Welcome 👋`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <main>
-          <Redirect to="/docs/home/introduction" />
+        <Redirect to="/docs/home/home/introduction" />
       </main>
     </Layout>
   );
